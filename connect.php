c48c833bf1d4586b6cf9aff81e04235299267555
Alfred Celes Hasa <abhasa@bounty.com.ph>
2025-12-11 15:40:58 +0800

    Add database connection script

---
+++ connect.php
@@ -0,0 +1,16 @@
+
+<?php
+$servername = "10.2.0.57";
+$username = "git_training";
+$password = "nVnr93?c";
+$dbname = "git_training";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
